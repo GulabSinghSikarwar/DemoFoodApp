69621ce6d2e7eda2bd2d5b9d2647458e1180edba
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2021-12-20 08:01:18 +0530

    Header .js

---
+++ demo-food-app/src/components/Layouts/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import classes from './Header.module.css'
+
+const Header =()=>{
+    return(
+        <Fragment>
+            <header>
+                <h1>React Meals </h1>
+                <button>
+                    cart
+                </button>
+
+            </header>
+            <div>
+
+
+            </div>
+        </Fragment>
+    )
+
+}
+export default Header;
